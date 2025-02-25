6ebbc9eb0e350e434601683f15d1317b6b0e4b3f
Privet906 <brianoconar1978@gmail.com>
2025-02-25 15:29:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,13 +6,13 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID === undefined ? 'put your session id' : process.env.SESSION_ID,
+    SESSION_ID: process.env.SESSION_ID === undefined ? 'PRABATH-MD~zSQwnaaD#0T4TUNqwanNEOhd2c-MbZrNL1Rt9Qcckor2q2y0BZQs' : process.env.SESSION_ID,
     PREFIX: process.env.PREFIX || '.',
     FOOTER: process.env.FOOTER || "*✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦*",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/za6ytm.jpg",
     PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 HEART_REACT: process.env.HEART_REACT || 'false',
-    SUDO: process.env.SUDO === undefined ? '94741671668,94771820962' : process.env.SUDO,
+    SUDO: process.env.SUDO === undefined ? '94784337506' : process.env.SUDO,
     ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello, I am alive now!!*" : process.env.ALIVE_MSG,
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ? "true" : process.env.AUTO_READ_STATUS,
     MODE: process.env.MODE === undefined ? "public" : process.env.MODE,
@@ -22,7 +22,7 @@ HEART_REACT: process.env.HEART_REACT || 'false',
     ANTI_DELETE: process.env.ANTI_DELETE === undefined ? "false" : process.env.ANTI_DELETE,
     ANTI_BOT: process.env.ANTI_BOT === undefined ? "false" : process.env.ANTI_BOT,
     READ_CMD: process.env.READ_CMD === undefined ? "false" : process.env.READ_CMD,
-    RECORDING: process.env.RECORDING === undefined ? "false" : process.env.RECORDING,
+    RECORDING: process.env.RECORDING === undefined ? "true" : process.env.RECORDING,
     AI_CHAT: process.env.AI_CHAT === undefined ? "false" : process.env.AI_CHAT,
     AUTO_SONG_SENDER: process.env.AUTO_SONG_SENDER === undefined ? "false" : process.env.AUTO_SONG_SENDER
 };
